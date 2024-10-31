d9c209c6ab73c3f615b4157f1b9d27daf3f76d0a
AmitPandey-106 <arprs9076@gmail.com>
2024-10-31 17:54:15 +0530

    third commit

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoUrl:"mongodb+srv://elite9594:bMVvLin4YvbNxMjB@anime.w4mlk.mongodb.net/animedata?retryWrites=true&w=majority&appName=anime",
+    //mongoUrl:"mongodb+srv://elite9594:bMVvLin4YvbNxMjB@anime.w4mlk.mongodb.net/animedata?retryWrites=true&w=majority&appName=anime",
     jwt_secret :"asdfghjklzxc", 
     CLOUDINARY_CLOUD_NAME:"dbpd5ykzl",
     CLOUDINARY_API_KEY:"318377384612493",
